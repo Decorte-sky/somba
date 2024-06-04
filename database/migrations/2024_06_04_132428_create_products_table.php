8fa97d34924c4ea48b03265ef06387709e2ec9d3
decorte <accelgaming60@gmail.com>
2024-06-04 16:00:32 +0200

    first commit

---
+++ database/migrations/2024_06_04_132428_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\Category;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class);
+            $table->foreignIdFor(Category::class);
+            $table->string("name");
+            $table->text("description");
+            $table->double("price");
+            $table->string("image");
+            $table->string("images");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
